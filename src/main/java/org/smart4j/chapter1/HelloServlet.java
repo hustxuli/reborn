d388b29b6205a4193bbacb03ab09539c8f126e5d
狼胥 <sherry.xl@alibaba-inc.com>
2018-01-07 13:36:27 +0800

    test

---
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,32 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package org.smart4j.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author hustxuli
+ * @version $Id: HelloServlet.java, v 0.1 2018年01月06日 下午4:03 hustxuli Exp $
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime",currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+}
---
+++ src/main/webapp/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hustxuli
+  Date: 18/1/6
+  Time: 下午4:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+<h1>hello!</h1>
+<h2>当前时间：${currentTime}</h2>
+</body>
+</html>
